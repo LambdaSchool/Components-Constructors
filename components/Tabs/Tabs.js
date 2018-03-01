43e0c504dcd0423a1d5aaa7abb558ee28e3b0bfd
Juber Ahmed <a.juber@hotmail.com>
2018-03-01 15:57:02 -0500

    Working on tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,8 +1,7 @@
-
 class TabsItem {
   constructor(element) {
     // attach dom element to object. Example in Tabs class
-    
+    this.element = element;
   }
 
   select() {
@@ -63,6 +62,7 @@ class Tabs {
   }
 
 }
+
 
 let tabs = document.querySelectorAll(".Tabs");
 tabs = Array.from(tabs).map(tabs => new Tabs(tabs));

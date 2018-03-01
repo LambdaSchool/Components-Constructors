aa8ea56f79587cda1f12e600e67a654b50c5e6a1
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-03-01 15:42:13 -0500

    Initial commit, working on js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,23 +1,26 @@
 
 class TabsItem {
   constructor(element) {
+    this.element = element;
     // attach dom element to object. Example in Tabs class
   }
 
   select() {
+    this.element.classList.add("Tabs__item-selected");
     // should use classList
   }
 
   deselect() {
+    this.element.classList.remove("Tabs__item-selected");
     // should use classList
   }
 }
 
 class TabsLink {
   constructor(element, parent) {
-    this.element;// attach dom element to object
-    this.tabs;// attach parent to object
-    this.tabsItem;// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
+    this.element = element;// attach dom element to object
+    this.tabs = parent;// attach parent to object
+    this.tabsItem = parent.getTab;// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
     // reassign this.tabsItem to be a new instance of TabsItem, passing it this.tabsItem
     this.element.addEventListener('click', () => {
       this.tabs.updateActive(this);
@@ -48,6 +51,7 @@ class Tabs {
   }
 
   init() {
+    this.links[0];
     // select the first link and tab upon ititialization
   }
 

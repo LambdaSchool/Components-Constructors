d4c10d00699410aa9b642e8b542c9341be954e8d
Erik Alonso <mr.alonsoea@protonmail.ch>
2018-03-01 18:31:21 -0600

    code set up by Erik Alonso

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,23 +1,26 @@
-
+/**********************************TABSITEM CLASS********************************/
 class TabsItem {
   constructor(element) {
     // attach dom element to object. Example in Tabs class
+    this.element = element;
   }
 
   select() {
     // should use classList
+    this.element.classList.add("Tabs__item-selected");
   }
 
   deselect() {
     // should use classList
+    this.element.classList.remove("Tabs__item-selected");
   }
 }
-
+/**********************************TABSLINK CLASS********************************/
 class TabsLink {
   constructor(element, parent) {
-    this.element;// attach dom element to object
-    this.tabs;// attach parent to object
-    this.tabsItem;// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
+    this.element = element;// attach dom element to object
+    this.tabs = parent;// attach parent to object
+    this.tabsItem = parent.getTab(element.dataset.tab);// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
     // reassign this.tabsItem to be a new instance of TabsItem, passing it this.tabsItem
     this.element.addEventListener('click', () => {
       this.tabs.updateActive(this);
@@ -28,27 +31,31 @@ class TabsLink {
   select() {
     // select this link
     // select the associated tab
+    this.element.classList.add("Tabs__link-selected");
   }
 
   deselect() {
     // deselect this link
     // deselect the associated tab
+    this.element.classList.remove("Tabs__link-selected");
   }
 }
-
+/**********************************TABS CLASS************************************/
 class Tabs {
   constructor(element) {
     this.element = element;// attaches the dom node to the object as "this.element"
-    this.links = element.querySelectorAll(".Tabs__link");
-    this.links = Array.from(this.links).map((link) => {
-      return new TabsLink(link, this);
-    });
-    this.activeLink = this.links[0];
-    this.init();
+    this.links = element.querySelectorAll(".Tabs__link"); // **this.links NOW HOLDS A NODELIST
+    this.links = Array.from(this.links).map((link) => { // **using Array.from BECAUSE I HAVE AN ARRAY-LIKE OR ITERABLE OBJECT
+      return new TabsLink(link, this); // ** link REPRESENTS EACH ITERABLE FROM NODELIST!
+    }); // **link CHANGES BUT NOT this. this IS THE PARENT for TabsLink
+    this.activeLink = this.links[0]; // ** I AM JUST STATING HERE THAT THE FIRST NODELIST ELEMENT IS MY SO-CALLED activeLink 
+    this.init(); // CALLING init() FUNCTION UPON CONTRUCTOR EXECUTION
   }
 
   init() {
     // select the first link and tab upon ititialization
+    let a = document.querySelectorAll(".Tabs__link");
+    a[0].setAttribute("display", "block");
   }
 
   updateActive(newActive) {
@@ -58,9 +65,10 @@ class Tabs {
 
   getTab(data) {
     // use the tab item classname and the data attribute to select the proper tab
+    return this.element.querySelector(`.Tabs__item[data-tab="${data}"]`);
   }
 
 }
-
-let tabs = document.querySelectorAll(".Tabs");
+/********************************************************************************/
+let tabs = document.querySelectorAll(".Tabs"); // tabs is a variable. ".Tabs" is being selected and assigned to tabs. querySelectorAll RETURNS A NODELIST, A LIST OF CHILD ELEMENTS AND IT'S DIFFERENT THAN CLASS LIST
 tabs = Array.from(tabs).map(tabs => new Tabs(tabs));

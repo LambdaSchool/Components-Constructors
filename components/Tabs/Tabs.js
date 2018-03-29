2eba572348e3867555094f8858a936dcfe938fb6
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-03-29 16:33:45 -0400

    Robert Hogan, David Finley. Components-Constructors project. Addition of previous project code. Progress on project.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,23 +2,27 @@
 class TabsItem {
   constructor(element) {
     // attach dom element to object. Example in Tabs class
+    this.element = element;
   }
 
   select() {
     // should use classList
+    this.element.classList.add("Tabs__Item-selected");
   }
 
   deselect() {
     // should use classList
+    this.element.classList.remove("Tabs__item-selected");
   }
 }
 
 class TabsLink {
   constructor(element, parent) {
-    this.element;// attach dom element to object
-    this.tabs;// attach parent to object
-    this.tabsItem;// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
+    this.element = element;// attach dom element to object
+    this.tabs = parent;// attach parent to object
+    this.tabsItem = parent.getTab(this.element.dataset.tab);// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
     // reassign this.tabsItem to be a new instance of TabsItem, passing it this.tabsItem
+    this.tabsItem = new TabsItem(this.tabsItem);
     this.element.addEventListener('click', () => {
       this.tabs.updateActive(this);
       this.select();
@@ -28,6 +32,8 @@ class TabsLink {
   select() {
     // select this link
     // select the associated tab
+    this.element.classList.add("Tabs__Link-selected");
+    this.tabsItem.select();
   }
 
   deselect() {

9438b21a255909ef0342224934877339d531a197
Jonathan Greene <greeneselectronics@gmail.com>
2018-03-29 14:00:48 -0600

    Combined previous days work with current

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,3 +1,35 @@
+console.log('I am being called');
+
+
+let dropdown = document.querySelector('.header__button');
+
+class Dropdown {
+    constructor (element) {
+        this.element = element;
+        this.element.addEventListener('click', () => {
+            this.dropdownOpen();
+        })
+    }
+    dropdownOpen() {
+        //console.log('I am being called!');
+        let list = document.querySelector('.header__list');
+        // list.style.display = 'block';
+        if (list.style.display === "none") {
+            list.style.display = "block";
+        } else {
+            list.style.display = "none";
+        }
+    };
+    
+}
+
+
+
+
+
+dropdown = new Dropdown(dropdown); // this creates an object 
+
+//////////////////////////////////////////////////////////////////////////////////////////
 
 class TabsItem {
   constructor(element) {

adc91b0e8b0d8b45f5b0819055435ce58dadbce0
imsdhk <jappleleaf@gmail.com>
2018-03-29 16:23:18 -0400

    finished the TabsLink class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,8 +21,9 @@ class TabsLink {
   constructor(element, parent) {
     this.element = element;// attach dom element to object
     this.tabs = parent;// attach parent to object
-    this.tabsItem = parent.getTab();// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
+    this.tabsItem = parent.getTab(this.element.dataset.tab);// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
     // reassign this.tabsItem to be a new instance of TabsItem, passing it this.tabsItem
+    this.tabsItem = new TabsItem(this.tabsItem);
     this.element.addEventListener('click', () => {
       this.tabs.updateActive(this);
       this.select();
@@ -31,12 +32,16 @@ class TabsLink {
 
   select() {
     // select this link
+    this.element.classList.add('Tabs__link-selected')
     // select the associated tab
+    this.tabsItem.select();
   }
 
   deselect() {
     // deselect this link
+    this.element.classList.remove('Tabs__link-selected')
     // deselect the associated tab
+    this.tabsItem.deselect();
   }
 }
 
@@ -65,6 +70,7 @@ class Tabs {
 
   getTab(data) {
     // use the tab item classname and the data attribute to select the proper tab
+    return this.element.querySelector(`.Tabs__item[data-tab="${data}"]`);
   }
 
 }

52446043d88d3cb231fb4109c8e2a9bec6220a9d
totsuya <supertatortots@gmail.com>
2018-03-29 16:06:41 -0400

    rewrote some stuff

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,3 +3,16 @@ class TabsItem {
     // attach dom element to object. Example in Tabs class
     this.element = element;
   }
+
+
+
+  select() {
+    // should use classList
+    element.classList.add("Tabs__item-selected"); //should reveal itself upon being clicked
+  }
+
+  deselect() {
+    // should use classList
+    element.classList.remove("Tabs__item-selected"); //should hide itself upon being unclicked
+  }
+}

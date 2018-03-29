b5637f6707f7ea4b1a8ee28ef6485dcf8110aeae
2940cristian <2940cristian@gmail.com>
2018-03-29 13:41:42 -0700

    worked on TabsLink class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -34,32 +34,39 @@ dropdown = new Dropdown(dropdown); // this creates an object
 class TabsItem {
   constructor(element) {
     // attach dom element to object. Example in Tabs class
+    this.links = element.querySelectorAll(".Tabs__item");
   }
 
   select() {
     // should use classList
+    this.element.classList.toggle = "Tabs__item-selected"
   }
 
   deselect() {
     // should use classList
+    this.element.classList.toggle = "Tabs__item"
   }
 }
 
 class TabsLink {
   constructor(element, parent) {
-    this.element;// attach dom element to object
-    this.tabs;// attach parent to object
-    this.tabsItem;// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
-    // reassign this.tabsItem to be a new instance of TabsItem, passing it this.tabsItem
+    this.element = element;// attach dom element to object
+    this.tabs = new Tabs(this.tabs);// attach parent to object
+    // assign this to the associated tab using the parent's "getTab" method by passing it the correct data
+    this.tabsItem = document.querySelector(`tab.[data-tab="${this.dataTab}"]`);
     this.element.addEventListener('click', () => {
       this.tabs.updateActive(this);
       this.select();
     });
+       // reassign this.tabsItem to be a new instance of TabsItem, passing it this.tabsItem
+    this.tabsItem = new TabsItem(this.tabsItem);
   };
 
   select() {
     // select this link
+    this.links.classList.toggle = 'Tabs__link-selected';
     // select the associated tab
+    this.links.classList.toggle = 'Tabs__link-selected';
   }
 
   deselect() {
@@ -96,3 +103,9 @@ class Tabs {
 
 let tabs = document.querySelectorAll(".Tabs");
 tabs = Array.from(tabs).map(tabs => new Tabs(tabs));
+
+let items = document.querySelectorAll(".Tabs__item");
+items = Array.from(items).map(items => new TabsItem(items));
+
+let link = document.querySelectorAll(".Tabs__link");
+link = Array.from(link).map(link => new Tabs(link));

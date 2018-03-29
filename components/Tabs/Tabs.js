08bd41b7e7396f96a9a8acf2f8d0ba5b584ec33f
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-29 19:56:46 -0400

    finalized tab functionality

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -28,14 +28,14 @@ class TabsLink {
   select() {
     // select this link
     // select the associated tab
-    this.element.classList.add('Tabs__item--selected');
+    this.element.classList.add('Tabs__link-selected');
     this.tabsItem.select();
   }
 
   deselect() {
     // deselect this link
     // deselect the associated tab
-    this.element.classList.remove('Tabs__item-selected');
+    this.element.classList.remove('Tabs__link-selected');
     this.tabsItem.deselect();
   }
 }

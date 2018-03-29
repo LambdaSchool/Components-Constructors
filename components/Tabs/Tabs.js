f57eb0c72f5cf91de545032575479268a56c6c6c
Jared Cuffe <jcuffe@gmail.com>
2018-03-29 13:08:42 -0700

    Stretch goal complete! I much prefer this type of organization

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,12 +1,10 @@
 
 class TabsItem {
   constructor(element) {
-    // attach dom element to object. Example in Tabs class
     this.element = element;
   }
 
   select() {
-    // should use classList
     this.element.classList.add('tabs__item--active');
   }
 
@@ -17,56 +15,54 @@ class TabsItem {
 }
 
 class TabsLink {
-  constructor(element, parent) {
+  constructor(element) {
     this.element = element;
-    this.tabs = parent;
-    this.tabsItem = new TabsItem(this.tabs.getTab(this.element.dataset.tab));
-    
-    this.element.addEventListener('click', () => {
-      this.tabs.updateActive(this);
-      this.select();
-    });
   };
 
   select() {
     this.element.classList.add('tabs__link--active');
-    this.tabsItem.select();
   }
 
   deselect() {
     this.element.classList.remove('tabs__link--active');
-    this.tabsItem.deselect();
   }
 }
 
 class Tabs {
   constructor(element) {
-    this.element = element;// attaches the dom node to the object as "this.element"
-    this.links = element.querySelectorAll(".tabs__link");
-    this.links = Array.from(this.links).map((link) => {
-      return new TabsLink(link, this);
+    this.element = element;
+    this.links = Array.from(element.querySelectorAll('.tabs__link')).map((link) => new TabsLink(link));
+    this.items = Array.from(element.querySelectorAll('.tabs__item')).map((item) => new TabsItem(item));
+    
+    this.links.forEach((link) => {
+      link.element.addEventListener('click', (event) => this.select(link.element.dataset.tab));
     });
-    this.activeLink = this.links[0];
+
     this.init();
+
   }
 
   init() {
     // select the first link and tab upon ititialization
-    this.activeLink.select();
+    this.select(1);
   }
 
-  updateActive(newActive) {
-    // deselect the old active link
-    // assign the new active link
-    this.activeLink.deselect();
-    this.activeLink = newActive;
+  select(tab) {
+    this.links.forEach((link) => {
+      if (link.element.dataset.tab == tab) {
+        link.select();
+      } else {
+        link.deselect();
+      }
+    });
+    this.items.forEach((item) => {
+      if (item.element.dataset.tab == tab) {
+        item.select();
+      } else {
+        item.deselect();
+      }
+    });
   }
-
-  getTab(data) {
-    // use the tab item classname and the data attribute to select the proper tab
-    return document.querySelector(`.tabs__item[data-tab="${data}"]`);
-  }
-
 }
-console.log('made it here');
+
 let tabs = Array.from(document.querySelectorAll(".tabs")).map(tabs => new Tabs(tabs));

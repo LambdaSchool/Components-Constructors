93e474007cff02a8465953206d46cf8a3ec5a5d5
EricGomez101 <eric_gomez_1@hotmail.com>
2018-03-29 21:35:15 -0700

    im just about done with the functionality of the buttons, the styling is sub par so far, still a WIP

--- scripts.js
+++ scripts.js
@@ -5,14 +5,22 @@ class btnToObj {
     this.element = element;
     this.data = this.element.dataset.number;
     this.element.addEventListener('click', () => {
-      this.buttonClick();
+      if (lastClick !== this.element || this.element.dataset.number === "1") {
+        this.buttonClick();
+      }
     });
     this.box = document.querySelector(`div[data-number='${this.data}']`);
     this.box = new Box(this.box);
   }
   buttonClick() {
+    if (this.element.dataset.number !== '1') {
+      lastClick.classList.remove('selected');
+      lastClick.classList.add('unselected');
+      document.querySelector(`div[data-number="${lastClick.dataset.number}"]`).classList.add('Container--displayNone');
+      lastClick = this.element;
+      lastClick.classList.add('selected');
+    }
     this.box.toggleBox();
-    console.log(this.box);
   }
 }
 
@@ -21,7 +29,12 @@ class Box {
     this.element = element;
   }
   toggleBox() {
-    this.element.classList.toggle("Container--displayNone");
+    if (this.element.dataset.number === '1') {
+      this.element.classList.toggle("Container--displayNone");
+    } else {
+    this.element.classList.remove("Container--displayNone");
+    }
   }
 }
 btn = Array.from(btn).map((btn) => new btnToObj(btn));
+let lastClick = document.querySelector('.BtnContainer__dropdownbtn[data-number="2"]')

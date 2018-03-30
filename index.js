28bc75849a1c9087b91d5957503bee617f41e0fc
hilal aissani <hilalaissani@gmail.com>
2018-03-30 00:42:00 -0400

    project 90%

--- index.js
+++ index.js
@@ -12,14 +12,21 @@ class Tabslink {
         this.index = index;
         this.dataNumber = element.dataset.number;
         this.element = element;
-        this.element.addEventListener('mousedown',() => {
+        this.element.addEventListener('click',() => {
             this.toggleTabsLink();
+        });
+        this.element.addEventListener('mouseleave', () => {
+            this.toggleTabsLink2();
         });
         this.tabsItem = document.querySelectorAll('.Tabs__item');
         this.tabsItem1 = Array.from(this.tabsItem).map((params, index) => new TabsItem(params, index));
     }
+    toggleTabsLink2(){
+    this.tabsItem1[this.index].tabsItems2();
+    }
     toggleTabsLink() {
         this.tabsItem1[this.index].TabsItem1();
+        
     }
 }
 class TabsItem {
@@ -28,8 +35,11 @@ class TabsItem {
         this.element = element;
         this.dataNumber = element.dataset.number;
     }
+    tabsItems2(){
+        this.element.classList.remove('Tabs__item-selected'); 
+    }
     TabsItem1() {   
-        this.element.classList.toggle('Tabs__item-selected');       
+        this.element.classList.toggle('Tabs__item-selected');     
     }
 }
 let constructedTabsLink = Array.from(tabsLink).map((param, index) => new Tabslink(param, index));
